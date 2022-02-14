1b1363da36ef2d3259aec1e815a355a88ac550f4
Mike Fitzpatrick <mlfitz2@gmail.com>
2022-02-14 12:44:13 -0800

    contact me section structured and functional

--- js/script.js
+++ js/script.js
@@ -1,41 +1,41 @@
 const aboutMeBtn = document.getElementById('about-me-btn');
 const myPortfolioBtn = document.getElementById('portfolio-btn');
 const contactMeBtn = document.getElementById('contact-me-btn');
-const myResumeBtn = document.getElementById('resume-btn');
 const aboutMe = document.getElementById('about-me');
 const myPortfolio = document.getElementById('my-portfolio');
 const contactMe = document.getElementById('contact-me');
-const myResume = document.getElementById('my-resume');
 const intro = document.getElementById('intro');
 
-
-//Combines two side navbars into one (at the top for mobile view)
-function moveNavbar() {
-    const size = window.matchMedia('(max-width: 808px)')
-    if (size.matches) {
-        document.getElementById('nav1').append(document.getElementById('contact-me-btn'));
-        document.getElementById('nav1').append(document.getElementById('resume-btn'));
-    }
-}
-
+//Renders About Me section
 function showAboutMe() {
     intro.setAttribute('class', 'hidden');
     myPortfolio.setAttribute('class', 'hidden');
     contactMe.setAttribute('class', 'hidden');
-    myResume.setAttribute('class', 'hidden');
     aboutMe.removeAttribute('class', 'hidden');
     
     aboutMeBtn.setAttribute('class', 'nav-btn selected');
     myPortfolioBtn.setAttribute('class', 'nav-btn');
     contactMeBtn.setAttribute('class', 'nav-btn');
-    myResumeBtn.setAttribute('class', 'nav-btn');
 }
 
+//Renders Contact Me section
+function showContactMe() {
+    intro.setAttribute('class', 'hidden');
+    myPortfolio.setAttribute('class', 'hidden');
+    contactMe.removeAttribute('class', 'hidden');
+    aboutMe.setAttribute('class', 'hidden');
+    
+    aboutMeBtn.setAttribute('class', 'nav-btn');
+    myPortfolioBtn.setAttribute('class', 'nav-btn');
+    contactMeBtn.setAttribute('class', 'nav-btn selected');
+}
+
+//Event listener: About Me section
 aboutMeBtn.addEventListener('click', function() {
     showAboutMe();
 });
 
-//Event listener - on loading, moves the two navbars together on mobile screens
-window.addEventListener('load', function() {
-    moveNavbar();
-});
+//Event listener: Contact Me section
+contactMeBtn.addEventListener('click', function(){
+    showContactMe();
+})
